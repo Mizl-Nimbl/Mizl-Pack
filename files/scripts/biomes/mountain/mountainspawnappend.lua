bf1b611841c79f34e3b0a75b50da28211b10b816
Mizl-Nimbl <110634762+Mizl-Nimbl@users.noreply.github.com>
2023-08-28 12:27:21 -0500

    Toy Meatworm
    added toy meatworm

--- files/scripts/biomes/mountain/mountainspawnappend.lua
+++ files/scripts/biomes/mountain/mountainspawnappend.lua
@@ -2,7 +2,15 @@ table.insert(g_cartlike,
 {
     prob           = 0.25,
     min_count    = 1,
-    max_count    = 1,    
-    offset_y 	= -5,   
+    max_count    = 1,
+    offset_y 	= -5,
     entity     = "data/entities/props/bnuuy.xml"
 })
+table.insert(g_cartlike,
+{
+    prob           = 0.25,
+    min_count    = 1,
+    max_count    = 1,
+    offset_y 	= -5,
+    entity     = "data/entities/props/meatworm.xml"
+})"
